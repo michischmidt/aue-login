de0d6dcc1f881580f9514abab4e0b0519e117d95
michischmidt <schmidt.m.dev@gmail.com>
2021-06-24 10:35:54 +0200

    first login screen and lint changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
+import SignIn from 'components/SignIn'
 
 export default function App () {
   return (
@@ -10,6 +11,7 @@ export default function App () {
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example
         </Typography>
+        <SignIn/>
       </Box>
     </Container>
   )
